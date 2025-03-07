54688b2657eef9978ea246a193a942b48ae674fd
carnoval15 <carnoval15@protonmail.com>
2025-03-07 19:25:24 +0330

    fixed a bug

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -12,7 +12,7 @@ export function AnimatedTestimonialsDemo() {
         },
         {
             quote:
-                "F WINDOWS!",
+                "F WINDOWS!                                                                       ",
             name: "Arshia",
             designation: "Founder at RPGames",
             src: "https://yqxicxlxliqzxrzajydh.supabase.co/storage/v1/object/sign/Gallery/Profiles/arshia.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJHYWxsZXJ5L1Byb2ZpbGVzL2Fyc2hpYS53ZWJwIiwiaWF0IjoxNzM4Mzg3NDcwLCJleHAiOjMzMjQyODUxNDcwfQ.HBrd0KVzP7BKni4W1qw7ou99fkZBxPO3tMIEEUIEzJA",
